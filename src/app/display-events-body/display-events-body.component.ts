da05004da86e2190dc7be79373e1dc04233ef38a
denizturgut99 <denizturgut1999@hotmail.com>
2020-01-05 15:09:50 +0100

    fixed the style.display mistake

--- src/app/display-events-body/display-events-body.component.ts
+++ src/app/display-events-body/display-events-body.component.ts
@@ -10,22 +10,25 @@ export class DisplayEventsBodyComponent {
 
   @Input() eventData;
   modalEvt;
-  lastJoinedEvent
+  timeoutEvent;
+  //lastJoinedEvent
 
-  constructor(private router: Router) {  }
+  constructor(private router: Router) { }
 
   toggleModalEvent(event) {
     this.modalEvt = Array(event);
   }
 
   getTriggeredEvent(event) {
-    this.lastJoinedEvent = event;
-    if(this.router.url === "/allEvents" || this.router.url === "/") {
-      document.getElementById("lastTriggeredEvent").innerHTML = "You have joined: " + event;
-      setTimeout(function() { document.getElementById("lastTriggeredEvent").style.display = "none" }, 3000);
+    //this.lastJoinedEvent = event;
+    document.getElementById("lastEvent").style.display = "block";
+
+    if (this.router.url === "/allEvents" || this.router.url === "/") {
+      document.getElementById("lastEvent").innerHTML = "You have joined: " + event;
+      setTimeout(function () { document.getElementById("lastEvent").style.display = "none" }, 3000);
     } else if (this.router.url === "/signedEvents") {
-     document.getElementById("lastTriggeredEvent").innerHTML = "You have left: " + event;
-     setTimeout(function() { document.getElementById("lastTriggeredEvent").style.display = "none" }, 3000);
+      document.getElementById("lastEvent").innerHTML = "You have left: " + event;
+      setTimeout(function () { document.getElementById("lastEvent").style.display = "none" }, 3000);
     }
     //setTimeout(document.getElementById("lastJoinedEvent").innerHTML = "You have joined: " + event, 3000);
   }
